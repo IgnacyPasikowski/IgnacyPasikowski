61e669d060503d7c651cda4efd0a14e54f94ba5d
IgnacyPasikowski <48653493+IgnacyPasikowski@users.noreply.github.com>
2025-05-18 13:02:30 +0200

    initial product scraping script with Google Sheets integration
    Scrapes selected product data (descriptions, images) from supplier website and exports to a structured Google Sheet, ready for import to final website. Permission obtained to reuse content.

---
+++ Gips/NordActions.py
@@ -0,0 +1,103 @@
+import gspread
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+
+class NordActions:
+    driver = None
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        chrome_options.add_argument('--headless=new')
+        chrome_options.add_argument("--window-size=1920,1080")
+        self.driver= webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+    def driver_get(self, URL):
+        self.driver.get(URL)
+        self.driver.maximize_window()
+
+    def crawl_through_cat(self):
+
+        # Category list for products
+        cat_list ="//div[@id='div_block-8-1280']/a[*]"
+        wait = WebDriverWait(self.driver, 15)
+        wait.until(EC.presence_of_element_located((By.XPATH, cat_list)))
+        list = self.driver.find_elements(By.XPATH,cat_list)
+        cat_urls = []
+        print(len(list))
+        for cat in list:
+            a = cat.get_attribute('href')
+            cat_name =cat.text
+            cat_urls.append([str(a),cat_name])
+            print(a,cat_name)
+        return cat_urls
+
+    def crawl_through_prod(self):
+        # Ten jest do gips
+        # prod_list = "//body/section[@id='section-2-1282']/div[1]/div[1]/div[2]/div[1]/ul[1]/li[*]/a[1]"
+        # ten jest do elewacyjnej
+        prod_list = "//body/section[@id='section-2-1291']/div[1]/div[1]/div[2]/div[1]/ul[1]/li[*]/a[1]"
+        next_page_btn = "//a[@class='next page-numbers']"
+        cat_xpath = "//h1[@class='page-title']"
+        wait = WebDriverWait(self.driver, 15)
+        cat_name = wait.until(EC.presence_of_element_located((By.XPATH, cat_xpath)))
+        cat = cat_name.text
+        product_url = [cat]
+        next = True
+        while next is True:
+            wait.until(EC.presence_of_element_located((By.XPATH, prod_list)))
+            list = self.driver.find_elements(By.XPATH, prod_list)
+            for product in list:
+                print(product)
+                a = product.get_attribute('href')
+                print(str(a))
+                product_url.append(str(a))
+            try:
+                next_page = self.driver.find_element(By.XPATH,next_page_btn)
+                next_page.click()
+                next = True
+            except:
+                print("Brak next buttona")
+                next = False
+
+        return(product_url)
+
+    def take_product(self,URL):
+        self.driver.get(URL)
+        self.driver.maximize_window()
+        prod_name = "//h1[@id='-product-title-10-1286']"
+        prod_desc = "//div[@id='-product-description-11-1286']"
+        prod_price = "//body[1]/section[4]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/p[1]/span[1]/bdi[1]"
+        wait = WebDriverWait(self.driver, 15)
+        name = wait.until(EC.presence_of_element_located((By.XPATH, prod_name)))
+        desc = wait.until(EC.presence_of_element_located((By.XPATH, prod_desc)))
+        price = wait.until(EC.presence_of_element_located((By.XPATH, prod_price)))
+        prod = [name.text,desc.text,price.text]
+        print(prod)
+        return(prod)
+
+    def take_product_photo(self,URL):
+
+        self.driver.get(URL)
+        self.driver.maximize_window()
+        prod_name = "//h1[@id='-product-title-10-1286']"
+        photo_set_url = "/html[1]/body[1]/section[4]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/a[1]/img[1]"
+        single_photo_url = "/html[1]/body[1]/section[4]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/img[1]"
+        wait = WebDriverWait(self.driver, 15)
+        name = wait.until(EC.presence_of_element_located((By.XPATH, prod_name)))
+        wait = WebDriverWait(self.driver, 2)
+        try:
+            photo = wait.until(EC.presence_of_element_located((By.XPATH, photo_set_url)))
+            photo = photo.get_attribute("srcset")
+
+        except TimeoutException:
+            photo = wait.until(EC.presence_of_element_located((By.XPATH, single_photo_url)))
+            photo = photo.get_attribute("src")
+        prod = [name.text,photo]
+        print(prod)
+        return(prod)
---
+++ Gips/ProductsUp.py
@@ -0,0 +1,58 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from NordActions import NordActions
+
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('keys.json', scope)
+client = gspread.authorize(creds)
+workbook = client.open("client")
+sheet = workbook.worksheet("xyz")
+
+    URL = "https://example.com/category/1"
+    NordActions = NordActions()
+    NordActions.driver_get(URL)
+    all_prod = []
+    category = NordActions.crawl_through_cat()
+    print(category)
+prod_data = []
+a = 1
+update_cells_list = []
+
+for cat in category:
+    if "Do montażu" in cat[1]:
+        print("tego nie chcemy")
+    else:
+        NordActions.driver_get(cat[0])
+        all_prod.append(NordActions.crawl_through_prod())
+for cat_list in all_prod:
+    i = 0
+    cat_name = cat_list[0]
+    for prod in cat_list:
+        if i == 0:
+            pass
+        else:
+            print("cat"+str(cat_name))
+            product = NordActions.take_product(prod)
+            product.extend([cat_name])
+            prod_data.append(product)
+            print(prod_data)
+        i = i + 1
+
+for prod in prod_data:
+    a = a + 1
+    output_row = a
+    cell_b = gspread.Cell(output_row, 1)
+    cell_c = gspread.Cell(output_row, 2)
+    cell_d = gspread.Cell(output_row, 3)
+    cell_e = gspread.Cell(output_row, 4)
+    cell_b.value = prod[0]
+    cell_c.value = prod[1]
+    cell_d.value = prod[2]
+    cell_e.value = prod[3]
+    update_cells_list.append(cell_b)
+    update_cells_list.append(cell_c)
+    update_cells_list.append(cell_d)
+    update_cells_list.append(cell_e)
+
+sheet.update_cells(update_cells_list, value_input_option="USER_ENTERED")
